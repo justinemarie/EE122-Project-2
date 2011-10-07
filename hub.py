2f384e6835d5126ffe712f8aff474d7200911423
Justine Sherry <stinie@gmail.com>
2011-10-07 01:25:01 -0700

    Initial assignment import.

---
+++ hub.py
@@ -0,0 +1,23 @@
+from sim.api import *
+from sim.basics import *
+import time
+
+class Hub (Entity):
+  """ A simple hub -- floods all packets """
+
+  def handle_rx (self, packet, port):
+    """
+    Just sends the packet back out of every port except the one it came
+    in on.
+    """
+    self.send(packet, port, flood=True)
+
+  def handle_link_up (self, port, entity):
+    """
+    Do nothing
+    """
+
+  def handle_link_down (self, port, entity):
+    """
+    Do nothing
+    """ 
